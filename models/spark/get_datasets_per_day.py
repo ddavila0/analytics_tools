b32c9964630b79e4dc9a6b12b8178e6e97221fc1
Diego Davila <diego.davila@cern.ch>
2019-07-12 00:13:14 +0200

    Adding spark files to model

---
+++ models/spark/get_datasets_per_day.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env spark-submit
+from __future__ import print_function
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession, Column
+from pyspark.sql.functions import col
+import pyspark.sql.functions as fn
+import pyspark.sql.types as types
+import schemas
+import sys
+
+year=sys.argv[1]
+month=sys.argv[2]
+
+inputfile="/project/monitoring/archive/condor/raw/metric/"+year+"/"+month+"/*/*.json.gz"
+outputfile="hdfs://analytix/user/ddavila/model/days_"+year+month+".parquet"
+
+print("===========================================================")
+print("reading: "+inputfile)
+print("writing: "+outputfile)
+print("===========================================================")
+
+conf = SparkConf().setMaster("yarn").setAppName("CMS Working Set")
+sc = SparkContext(conf=conf)
+spark = SparkSession(sc)
+
+# Get information from DBS about datatsets IDs
+csvreader = spark.read.format("com.databricks.spark.csv").option("nullValue","null").option("mode", "FAILFAST")
+dbs_datasets = csvreader.schema(schemas.schema_datasets()).load("/project/awg/cms/CMS_DBS3_PROD_GLOBAL/current/DATASETS/part-m-00000")
+
+schema = types.StructType([
+            types.StructField("data", types.StructType([
+                    types.StructField("Status", types.StringType(), True),
+                    types.StructField("Type", types.StringType(), True),
+                    types.StructField("JobUniverse", types.StringType(), True),
+                    types.StructField("DESIRED_CMSDataset", types.StringType(), True),
+                    types.StructField("RecordTime", types.LongType(), True),
+                 ]), False),
+            ])
+
+jobreports = spark.read.schema(schema).json(inputfile)
+working_set_day = (jobreports
+        .filter(col('data.Status')=="Completed")
+        .filter(col('data.Type')=="analysis")
+        .filter(col('data.JobUniverse')==5)
+        .filter(col('data.DESIRED_CMSDataset').isNotNull())
+        .withColumn('day_ts', (col('data.RecordTime')-col('data.RecordTime')%fn.lit(86400000))/fn.lit(1000))
+        .withColumn('week_ts', (col('data.RecordTime')-col('data.RecordTime')%fn.lit(604800000))/fn.lit(1000))
+        .join(dbs_datasets, col('data.DESIRED_CMSDataset')==col('d_dataset'))
+        .groupBy('day_ts', 'week_ts')
+        .agg(
+            fn.collect_set('d_dataset_id').alias('datasets_set'),
+        )
+    )
+
+working_set_day.write.parquet(outputfile)
+
