50e44d5227e7c178f8d3f241201c8af52e2d2e9a
Diego Davila <diego.davila@cern.ch>
2019-07-19 20:43:58 +0200

    Adding get_dataset_sizes.py

--- models/spark/get_months.sh
+++ models/spark/get_months.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in 06 07 08 09 10 11 12;
+#for i in 06 07 08 09 10 11 12;
+for i in 01 02 03 04 05;
 do
 spark-submit \
     --conf spark.driver.port=5001 \
@@ -7,5 +8,5 @@ spark-submit \
     --conf spark.ui.port=5201 \
     --conf spark.driver.extraClassPath='/eos/project/s/swan/public/hadoop-mapreduce-client-core-2.6.0-cdh5.7.6.jar' \
     --packages com.databricks:spark-avro_2.11:4.0.0 \
-    $@ 2018 $i;
+    $@ 2019 $i;
 done
